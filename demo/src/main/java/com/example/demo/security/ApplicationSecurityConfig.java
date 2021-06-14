a0962e6e8235a26a32df287d30ef1c7a8a4096ed
jeetpatel24 <friend.jeet24@gmail.com>
2021-06-14 12:00:04 +0530

    Roles and Permissions using ENUMS

--- demo/src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ demo/src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -12,6 +12,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import static com.example.demo.security.ApplicationUserRole.*;
+
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -38,17 +40,17 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     @Bean
     protected UserDetailsService userDetailsService() {
-        
+
         UserDetails annaSmithUser = User.builder()
                 .username("annasmith")
                 .password(passwordEncoder.encode("password"))
-                .roles("STUDENT")   //ROLE_STUDENT
+                .roles(STUDENT.name())   //ROLE_STUDENT    //ApplicationUserRole.STUDENT.name()
                 .build();
 
         UserDetails lindaUser = User.builder()
                 .username("linda")
                 .password(passwordEncoder.encode("password123"))
-                .roles("ADMIN")
+                .roles(ADMIN.name())    //ROLE_ADMIN    //ApplicationUserRole.ADMIN.name()
                 .build();
 
         return new InMemoryUserDetailsManager(
